97099b1884832fad20a750a9430030e492167eb1
kaya jepson and zsuzsanna mangu <kaya@>
2019-04-10 14:26:11 -0700

    add button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,51 @@
 // // Business logic
-function PlayerTotal() {
-  this.grandTotal = []
-  this.currentId = 0
+function Player(name) {
+  this.name = name;
+  this.tempScore = 0;
+  this.Score = 0;
+  // this.currentId = 0
 }
 
-PlayerTotal.prototype.addRoll = function(roll) {
-  grandTotal.id = this.assignId();
-  this.grandTotal.push(roll);
+
+Player.prototype.Roll = function() {
+  var number = diceRoll();
+  if (number === 1) {
+    this.tempScore = 0;
+    console.log("one", number);
+    // this.EndTurn()
+  } else {
+    this.tempScore += number
+    console.log("single", number);
+  }
+    console.log("total", this.tempScore);
 }
 
-PlayerTotal.prototype.assignId = function() {
-  this.currentId +=1;
-  return this.currentId;
+Player.prototype.Hold = function () {
+  this.Score += this.tempScore;
+  this.tempScore = 0;
+  console.log("all", this.Score);
+  // this.EndTurn()
 }
+
 
 //Business Logic for total -----
-// function SmallScore(rolledNumber, currentScore) {
-//   this.rolledNumber = rolledNumber
-//   this.currentScore = currentScore
-// }
-
-// SmallScore.prototype.
-//   return this.rolledNumber + this.currentScore;
-
 function diceRoll() {
   return (Math.floor((Math.random() * 6) + 1));;
 }
 
-var playerTotal = new PlayerTotal();
-var roll = ''
+
+// var roll = ''
 $(document).ready(function()  {
+  var newPlayer = new Player();
   $(".btn1").click(function() {
-    var roll = diceRoll();
-    console.log(roll);
+    newPlayer.Roll();
   });
   $(".btn2").click(function() {
-    PlayerTotal.addRoll(this.id);
-    console.log(this.grandTotal);
+    newPlayer.Hold();
   });
-  $(".btn3").click(function() {
-    var roll = diceRoll();
-    console.log(roll);
-  });
+  // $(".btn3").click(function() {
+  //   var roll = diceRoll();
+  //   console.log(roll);
+  // });
+  // console.log("all", this.Score);
 });
