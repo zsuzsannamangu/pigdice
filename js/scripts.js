0645469318fb36f1624a6e19d5847775b774eb6e
kaya jepson and zsuzsanna mangu <zsuzsanna@>
2019-04-10 15:39:56 -0700

    add everything

--- js/scripts.js
+++ js/scripts.js
@@ -8,25 +8,31 @@ function Player(name) {
 
 
 Player.prototype.Roll = function() {
+  // this.tempScore = 0;
   var number = diceRoll();
   if (number === 1) {
     this.tempScore = 0;
     console.log("one", number);
+    return 0
     // this.EndTurn()
   } else {
-    this.tempScore += number
     console.log("single", number);
+    return this.tempScore += number
   }
     console.log("total", this.tempScore);
 }
 
 Player.prototype.Hold = function () {
-  this.Score += this.tempScore;
-  this.tempScore = 0;
   console.log("all", this.Score);
-  // this.EndTurn()
+  return this.Score += this.tempScore;
+  this.EndTurn()
+  // this.tempScore = 0;
 }
 
+Player.prototype.EndTurn = function() {
+  this.tempScore = 0;
+  //change to next player show/hide
+}
 
 //Business Logic for total -----
 function diceRoll() {
@@ -37,11 +43,37 @@ function diceRoll() {
 // var roll = ''
 $(document).ready(function()  {
   var newPlayer = new Player();
+  var newPlayer2 = new Player();
   $(".btn1").click(function() {
-    newPlayer.Roll();
+    // newPlayer.Roll();
+    $(".endmessage2").hide();
+    // $(".player1-number").show();
+
+    var player1temp = newPlayer.Roll();
+    $(".player1temp").text(player1temp);
+
   });
   $(".btn2").click(function() {
-    newPlayer.Hold();
+    // newPlayer.Hold();
+    $(".endmessage1").show();
+
+    var player1number = newPlayer.Hold();
+    $(".player1number").text(player1number);
+
+  });
+  $(".btn3").click(function() {
+    // newPlayer2.Roll();
+    $(".endmessage1").hide();
+
+    var player2temp = newPlayer2.Roll();
+    $(".player2temp").text(player2temp);
+  });
+  $(".btn4").click(function() {
+    // newPlayer2.Hold();
+    $(".endmessage2").show();
+
+    var player2number = newPlayer2.Hold();
+    $(".player2number").text(player2number);
   });
   // $(".btn3").click(function() {
   //   var roll = diceRoll();
