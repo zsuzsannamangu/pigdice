a123cc7fe14b374541db4e97dedf966f858b01f0
zsuzsannamangu <zsuzsannamangu@gmail.com>
2019-04-11 17:32:09 -0700

    fix javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,58 +6,155 @@ function Player(name) {
   this.currentRoll = 0;
 }
 
-Player.prototype.Roll = function() {
-  var number = diceRoll();
-  if (number === 1) {
-    this.currentRoll = number;
-    // this.EndTurn();
-    return 0
+Player.prototype.roll = function() {
+  this.currentRoll = diceRoll();
+  if (this.currentRoll === 1) {
+    this.endTurn();
   } else {
-    this.currentRoll = number;
-    return this.tempScore += number
+    this.tempScore += this.currentRoll;
+  }
+  return this.currentRoll;
+}
 
+Player.prototype.hold = function () {
+  this.totalScore = this.Score += this.tempScore;
+  this.endTurn();
+  return this.totalScore;
+}
+
+Player.prototype.endTurn = function() {
+  this.tempScore = 0;
+  console.log('END TURN!!!', this.name);
+  this.game.nextPlayer();
+}
+
+
+
+function Game() {
+  this.players = [];
+  this.currentPlayerIndex = 0;
+}
+
+Game.prototype.addPlayer = function(player) {
+  this.players.push(player);
+  // Make this game accessable to the player
+  player.game = this;
+}
+
+Game.prototype.nextPlayer = function(){
+  var currentPlayer = this.players[this.currentPlayerIndex]
+  if (this.currentPlayerIndex === this.players.length - 1) {
+      this.currentPlayerIndex = 0
+  } else {
+    this.currentPlayerIndex++
   }
 }
-
-Player.prototype.Hold = function () {
-  var totalScore = this.Score += this.tempScore;
-  // this.EndTurn();
-  return totalScore
-
-}
-
-// Player.prototype.EndTurn = function() {
-//   this.tempScore = 0;
-//   // return 0;
-//   this.newGame.nextPlayer();
-// }
 //
-// function Game() {
-//   this.players = [];
-//   this.currentPlayerIndex = 0;
-// }
-//
-// Game.prototype.addPlayer = function(player) {
-//   this.players.push(player);
-//   player.game = this;
-// }
-//
-// Game.prototype.nextPlayer = function(){
-//   var currentPlayer = this.players[this.currentPlayerIndex]
-//   if (this.currentPlayerIndex === this.players.length - 1) {
-//       this.currentPlayerIndex = 0
-//     }
-//       else {
-//       this.currentPlayerIndex++
-//     }
-// }
+var zsuzsi = new Player('Zsuzsi');
+var ben = new Player('Ben');
+var me = new Player('me');
 
+var game = new Game();
+
+game.addPlayer(ben);
+game.addPlayer(zsuzsi);
+game.addPlayer(me);
+
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+
+console.log(game.players[game.currentPlayerIndex].roll(
+))
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+
+console.log(game.players[game.currentPlayerIndex].roll(
+))
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+
+console.log(game.players[game.currentPlayerIndex].roll(
+))
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+
+console.log(game.players[game.currentPlayerIndex].roll(
+))
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+
+console.log(game.players[game.currentPlayerIndex].hold(
+))
+console.log(game.players[game.currentPlayerIndex].name,
+   game.players[game.currentPlayerIndex].currentRoll,
+    game.players[game.currentPlayerIndex].tempScore,
+     game.players[game.currentPlayerIndex].Score)
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
+
+     console.log(game.players[game.currentPlayerIndex].roll(
+     ))
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
+
+     console.log(game.players[game.currentPlayerIndex].roll(
+     ))
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
+
+     console.log(game.players[game.currentPlayerIndex].roll(
+     ))
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
+
+     console.log(game.players[game.currentPlayerIndex].roll(
+     ))
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
+
+     console.log(game.players[game.currentPlayerIndex].hold(
+     ))
+     console.log(game.players[game.currentPlayerIndex].name,
+        game.players[game.currentPlayerIndex].currentRoll,
+         game.players[game.currentPlayerIndex].tempScore,
+          game.players[game.currentPlayerIndex].Score)
 
 //Business Logic for diceRoll -----
 function diceRoll() {
   return (Math.floor((Math.random() * 6) + 1));;
 }
 
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
+// console.log(zsuzsi.roll())
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
+// console.log(zsuzsi.roll())
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
+// console.log(zsuzsi.roll())
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
+// console.log(zsuzsi.roll())
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
+// console.log(zsuzsi.hold())
+// console.log(zsuzsi.name, zsuzsi.currentRoll, zsuzsi.tempScore, zsuzsi.Score)
 
 $(document).ready(function()  {
   // var newGame = new Game();
